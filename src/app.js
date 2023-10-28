632a250dc8b4f63caf5d1da891250e5e4b5a1363
Jose Dominguez <141463676+jdomi020@users.noreply.github.com>
2023-10-28 01:37:26 +0000

    redid my code and put in source app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const domainNameCombinations = generateDomainNames(fruits, animals, gems, cars);
+console.log(domainNameCombinations);
 };
+
+let fruits = ['apples', 'oranges', 'pears'];
+let animals = ['cat', 'dog', 'snakes'];
+let gems = ['ruby', 'sapphire', 'diamond'];
+let cars = ['Maserati', 'Mercedes', 'McLaren']
+
+function generateDomainNames(fruits, animals, gems, cars) {
+  let domainNames = [];
+
+  fruits.forEach((fruit) => {animals.forEach((animal) => {gems.forEach((gem) => {cars.forEach((car) => {const domainName = `${fruit}${animal}${gem}${car}.com`;
+  domainNames.push(domainName); }); });});});
+  return domainNames;
+}
